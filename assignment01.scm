87bda7d529e4796957945d7864bdc27b6259530c
Chuyang Chen <chuyangchen2018@outlook.com>
2022-10-15 20:58:18 +0800

    First commit

---
+++ assignment01.scm
@@ -0,0 +1,30 @@
+;;; Assignment01: Scheme
+
+(define (over-or-under a b)
+  'YOUR-CODE-HERE
+)
+
+
+(define (make-adder n)
+  'YOUR-CODE-HERE
+)
+
+
+(define (composed f g)
+  'YOUR-CODE-HERE
+)
+
+
+(define (remainder a b)
+  (- a (* b (quotient a b))))
+
+(define (gcd a b)
+  'YOUR-CODE-HERE
+)
+
+
+(define (square x) (* x x))
+
+(define (pow base exp)
+  'YOUR-CODE-HERE
+)
