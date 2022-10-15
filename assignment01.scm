783cc6d73466a9a8794bfbbda3b2d411c5356c3e
Chuyang Chen <chuyangchen2018@outlook.com>
2022-10-15 21:58:35 +0800

    Implement ok tests

--- assignment01.scm
+++ assignment01.scm
@@ -1,17 +1,17 @@
 ;;; Assignment01: Scheme
 
 (define (over-or-under a b)
-  'YOUR-CODE-HERE
+  (if (> a b) 1 (if (< a b) -1 0))  
 )
 
 
 (define (make-adder n)
-  'YOUR-CODE-HERE
+  (lambda (m) (+ n m))
 )
 
 
 (define (composed f g)
-  'YOUR-CODE-HERE
+  (lambda (x) (f (g x)))
 )
 
 
@@ -19,12 +19,14 @@
   (- a (* b (quotient a b))))
 
 (define (gcd a b)
-  'YOUR-CODE-HERE
+  (let ((a (if (> a b) a b)) (b (if (> a b) b a)))
+    (if (= (remainder a b) 0) b (gcd b (remainder a b)))
+  )
 )
 
 
 (define (square x) (* x x))
 
 (define (pow base exp)
-  'YOUR-CODE-HERE
+  (if (= exp 0) 1 (* base (pow base (- exp 1))))
 )
